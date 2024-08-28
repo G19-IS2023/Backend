c4593e7659429f74d94846affd462a08634f392d
Riccardo Patalocchi <79475389+rickypat03@users.noreply.github.com>
2024-08-29 00:03:20 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ import { config } from 'dotenv';
 
 config();
 
-const PORT = process.env.PORT || 5050;
+const PORT = (process.env.PORT || 5050) as number;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
