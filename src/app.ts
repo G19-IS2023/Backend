a99bc5e1046e6e288263be3847817c6036eca68f
Riccardo Patalocchi <79475389+rickypat03@users.noreply.github.com>
2024-08-29 00:52:05 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ dotenv.config({path: '../.env'});
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:',
+    origin: '*',
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
