c19289c10889b6032c9fdb10293ab59604c7dce5
Riccardo Patalocchi <79475389+rickypat03@users.noreply.github.com>
2024-09-02 15:37:04 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { userRouter } from './routes/userRoutes';
 import { bookRouter } from './routes/bookRoutes';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json';
 
 dotenv.config({path: '../.env'});
 
@@ -22,8 +20,6 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/user', userRouter);
 app.use('/book', bookRouter);
